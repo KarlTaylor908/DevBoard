8ecff98016ccbd2886fae90a1e14f5ab1e02666a
Karl Taylor <karltaylor908@yahoo.co.uk>
2025-08-06 20:47:19 +0100

    Board Ent Lowercase removed. Authorize tag added on Board Controller.

--- src/DevBoard.Domain/Boards/Entities/BoardEnt.cs
+++ src/DevBoard.Domain/Boards/Entities/BoardEnt.cs
@@ -1,5 +1,5 @@
-﻿using DevBoard.Domain.Auth.Entities;
-using DevBoard.Domain.Shared;
+﻿using DevBoard.Domain.Shared;
+using DevBoard.Domain.User.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
